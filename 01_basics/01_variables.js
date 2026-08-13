1129277f3457fd8ded2f29f539118a11ac011414
mokshbansal1429 <mokshbansal2905@gmail.com>
2026-08-13 05:31:28 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 133345
+let emailId="moksh@google.com"
+var accountPassword="12900"
+accountCity="mumbai"
+console.table([emailId,accountPassword,accountCity,accountId])
+//accountId=90876  not allowed
+emailId="mokshbansal@google.com"
+accountPassword="0099"
+accountCity="haryana"
+//console.log(accountId)
+/*
+prefer not to use var 
+beacause of issue in block scope and functional scope
+*/
+/*
+agr variable bnake chhor de mtlb value assign na ho to print hoga undefined
+
+*/
+let personName;
+console.table([emailId,accountPassword,accountCity,accountId,personName])
