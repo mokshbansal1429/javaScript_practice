b024754c40b7e51570ba3e6fe1f8e25546b8319c
mokshbansal1429 <mokshbansal2905@gmail.com>
2026-08-13 06:42:56 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "hitesh"
+console.log(typeof score);
+console.log(typeof (score))
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+
+let isLoggedIn=1 //true
+//  isLoggedIn=""    false
+// isLoggedIn = "MOksh"   true
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let number=33
+let stringNumber=String(number);
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
