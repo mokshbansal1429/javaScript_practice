be9f0082f21fdce856acafb307f1da7f85992d27
mokshbansal1429 <mokshbansal2905@gmail.com>
2026-08-13 06:06:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all JS code as a new version
+// alert(3+3) we are using node.js ,not browser
+console.log(3
+    +3
+)   // code readability should be higher
+console.log("moksh")
+
+
+
+
+let name="MOKSH" // string datatype
+let age=21 // int datatype
+let isLogged=true // boolean
+
+
+/*
+range number => 2 ki power 53
+bigint
+string => "" ya '' preffered ""
+boolean = true/false
+
+
+null => standalone value , e.g agr temp dekhna ho to 
+error aaye to null print kra skte h 
+
+undefined =>  abhi valued assign nhi hui h e.g let age;
+
+
+typeof =  type check krne ke kis type ka and 
+important thing is null ka type object aata h 
+and undefined ka undefined hi aata h
+
+
+symbol => uniqune define krne ke liye
+
+*/
+
+console.log(typeof "moksh")
+console.log(typeof null)
