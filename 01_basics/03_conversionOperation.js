9ac4275d03f6f5cd8a856e73dcc073fb629c872a
mokshbansal1429 <mokshbansal2905@gmail.com>
2026-08-14 19:55:46 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "hitesh"
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -15,10 +15,38 @@ let isLoggedIn=1 //true
 //  isLoggedIn=""    false
 // isLoggedIn = "MOksh"   true
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 let number=33
 let stringNumber=String(number);
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+//console.log(stringNumber)
+//console.log(typeof(stringNumber))
+
+
+
+
+
+
+
+
+
+
+//**************operations */
+
+
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+
+console.log(2**2) // power m 
+console.log("1" + 2)  //12
+console.log(1 + "2")   //12
+console.log(1 + 2 +"3")  // 33
+let x=10
+x++
+console.log(x) //11
+console.log(++x)  //12
+console.log(x++) //12
+console.log(x) //13
 
