a00de886dda70620e647b8590c97807d071e253a
mokshbansal1429 <mokshbansal2905@gmail.com>
2026-08-14 20:16:52 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,33 @@
+//BASIC COMPARIOSONS
+
+// console.log( 2 > 1)
+// console.log(2 == 2)
+// console.log( 2!= 1 )
+// console.log(4 < 6)
+
+
+
+//tricky
+
+// === y datatype bhi check krta h if different datatype then give false
+console.log("2" === 1)
+console.log(2 == "1")
+console.log ( "2" > 1)
+
+
+//confusing
+ 
+
+console.log ( null == 0)
+console.log(null > 0)
+console.log(null < 0)
+console.log(null <=0)
+console.log(undefined == 0)
+console.log(undefined > 0 )
+console.log(undefined <= 0)
+
+// IMP_NOTE => == m null ko null hi treate kr rhe h but jb comparisons use hota h to null 0 ki trh treate hota h
+// undefined ka hmesa false hi aayega
+
+// IMPORTANT => avoid these type of comparisons as clean code ia our motto
+
